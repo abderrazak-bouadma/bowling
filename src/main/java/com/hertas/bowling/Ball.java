08987df8ef976de28d4b52e6470444dac6a0d3d9
Abderrazak <bouadma.abderrazak@gmail.com>
2013-07-23 16:19:25 +0200

    .refactoring : Ball replaced by an enum instead of a class

--- src/main/java/com/hertas/bowling/Ball.java
+++ src/main/java/com/hertas/bowling/Ball.java
@@ -1,20 +1,35 @@
 package com.hertas.bowling;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Utilistateur
- * Date: 23/07/13
- * Time: 12:13
- * To change this template use File | Settings | File Templates.
- */
-public class Ball {
-    private final int knockedPins;
+public enum Ball {
+    ZERO_PIN_BALL(0),
 
-    public int getKnockedPins() {
-        return knockedPins;
+    ONE_PIN_BALL(1),
+
+    TWO_PIN_BALL(2),
+
+    THREE_PIN_BALL(3),
+
+    FOUR_PIN_BALL(4),
+
+    FIVE_PIN_BALL(5),
+
+    SIX_PIN_BALL(6),
+
+    SEVEN_PIN_BALL(7),
+
+    EIGHT_PIN_BALL(8),
+
+    NINE_PIN_BALL(9),
+
+    STRIKE_BALL(10);
+
+    private final int pinsKnockedOut;
+
+    Ball(int pinsKnockedOut) {
+        this.pinsKnockedOut = pinsKnockedOut;
     }
 
-    public Ball(int knockedPins) {
-        this.knockedPins = knockedPins;
+    public int getPinsKnockedOut() {
+        return pinsKnockedOut;
     }
 }
--- src/test/java/com/hertas/bowling/BowlingGameTest.java
+++ src/test/java/com/hertas/bowling/BowlingGameTest.java
@@ -3,31 +3,24 @@ package com.hertas.bowling;
 import junit.framework.Assert;
 import org.junit.Test;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Utilistateur
- * Date: 23/07/13
- * Time: 10:25
- */
 public class BowlingGameTest {
 
     @Test
     public void frame1_score_should_equals_5() {
-        BowlingGame game = BowlingGame.newGame();
-        game.play(new Ball(1)).play(new Ball(4));
+        BowlingGame game = BowlingGame.newGame().play(Ball.ONE_PIN_BALL).play(Ball.FOUR_PIN_BALL);
         int scoreByFrameIndex = game.getScoreByFrameIndex(1);
         Assert.assertEquals(5, scoreByFrameIndex);
     }
 
     @Test
     public void frame2_score_should_be_14() {
-        BowlingGame game = BowlingGame.newGame();
-        game.play(new Ball(1)).play(new Ball(4)).play(new Ball(4)).play(new Ball(5));
+        BowlingGame game = BowlingGame.newGame().play(Ball.ONE_PIN_BALL).play(Ball.FOUR_PIN_BALL).play(Ball.FOUR_PIN_BALL).play(Ball.FIVE_PIN_BALL);
         Assert.assertEquals(14, game.getScoreByFrameIndex(2));
     }
 
     @Test
     public void frame3_strike_calculate_score() {
-
+        BowlingGame game = BowlingGame.newGame();
+        game.play(Ball.ONE_PIN_BALL).play(Ball.FOUR_PIN_BALL).play(Ball.STRIKE_BALL).play(Ball.FOUR_PIN_BALL).play(Ball.SIX_PIN_BALL);
     }
 }
