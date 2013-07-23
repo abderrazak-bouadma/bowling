4855a6653a23ad02cfe1aaa79dcf222a03180e41
Abderrazak <bouadma.abderrazak@gmail.com>
2013-07-23 11:45:15 +0200

    first commit

---
+++ src/main/java/com/hertas/bowling/BowlingGame.java
@@ -0,0 +1,11 @@
+package com.hertas.bowling;
+
+public class BowlingGame {
+
+    public void play() {
+    }
+
+    public BowlingFrame getFrame(int i) {
+        return new BowlingFrame();
+    }
+}
